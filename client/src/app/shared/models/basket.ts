5f1fb1c7cb987a73a099a7ef9bbfe87f1f590082
kadirgulteekin <kadirgulteekin@gmail.com>
2022-12-24 16:50:10 +0300

    basket processing

--- client/src/app/shared/models/basket.ts
+++ client/src/app/shared/models/basket.ts
@@ -12,13 +12,13 @@ import { v4 as uuidv4 } from 'uuid';
 
   export interface IBasket {
       id: string;
-      items: IBasket[];
+      items: IBasketItem[];
   }
 
 
   export class Basket implements IBasket{
     id: uuidv4
-    items: IBasket[];
+    items: IBasketItem[]=[];
 
   }
 
