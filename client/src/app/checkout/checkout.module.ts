3892cfd7146219b6ceec9e5da1036cabd40cb279
kadirgulteekin <kadirgulteekin@gmail.com>
2023-01-23 01:04:42 +0300

    payment module created.

--- client/src/app/checkout/checkout.module.ts
+++ client/src/app/checkout/checkout.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CheckoutComponent } from './checkout.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CheckoutComponent
+  ],
   imports: [
     CommonModule
   ]
