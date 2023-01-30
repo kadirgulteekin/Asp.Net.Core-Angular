e319391a9f2b018a64f2db902e9f0bec911fab52
kadirgulteekin <kadirgulteekin@gmail.com>
2023-01-30 17:40:52 +0300

    Login and Register component done.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -18,6 +18,7 @@ const routes: Routes = [
   //  {path:"basket",component:BasketComponent,data:{breadcrumb:'basket'}},
   {path:"basket",loadChildren:()=>import('./basket/basket.module').then(mod=>mod.BasketModule),data:{breadcrumb:'Basket'}},
   {path:"checkout",loadChildren:()=>import('./checkout/checkout.module').then(mod=>mod.CheckoutModule),data:{breadcrumb:'Checkout'}},
+  {path:"account",loadChildren:()=>import('./account/account.module').then(mod=>mod.AccountModule),data:{breadcrumb:{skip:true}}},
 ];
 
 @NgModule({
