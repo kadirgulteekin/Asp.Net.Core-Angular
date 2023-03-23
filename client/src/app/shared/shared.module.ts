5fb6bd0089413e8d06f342c1ce85b7319ff42b39
kadirgulteekin <kadirgulteekin@gmail.com>
2023-03-23 10:25:34 +0300

    Reusable generic text input created

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -7,6 +7,7 @@ import { PagerComponent } from './components/pager/pager.component';
 import { OrderTotalsComponent } from './order-totals/order-totals.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import {  BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TextInputComponent } from './components/text-input/text-input.component';
 
 
 
@@ -14,7 +15,8 @@ import {  BsDropdownModule } from 'ngx-bootstrap/dropdown';
   declarations: [
     PagingHeaderComponent,
     PagerComponent,
-    OrderTotalsComponent
+    OrderTotalsComponent,
+    TextInputComponent
   ],
   imports: [
     CommonModule,
@@ -31,7 +33,9 @@ import {  BsDropdownModule } from 'ngx-bootstrap/dropdown';
   CarouselModule,
   OrderTotalsComponent,
   ReactiveFormsModule,
-  BsDropdownModule
+  BsDropdownModule,
+  TextInputComponent
+
 
 ]
 })
